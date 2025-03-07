a8334c69f69e33ecefcbe99355d0ce1ae6d8d012
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-07 17:57:48 +0900

    pagenation

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -41,6 +41,24 @@ class OwnerSeeder extends Seeder
                 'password' => Hash::make('password123'),
                 'created_at' => '2025/03/07 11:11:11'
             ],
+            [
+                'name' => 'test5',
+                'email' => 'test5@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2025/03/07 11:11:11'
+            ],
+            [
+                'name' => 'test6',
+                'email' => 'test6@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2025/03/07 11:11:11'
+            ],
+            [
+                'name' => 'test7',
+                'email' => 'test7@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2025/03/07 11:11:11'
+            ],
 
         ]);
     }
