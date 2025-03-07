417c7358e30b00f672ecc080703e001c9a56b055
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-07 14:08:52 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; // Eloquent エロくアント
+use Illuminate\Support\Facades\DB; // QueryBuilder
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+//    /**
+//     * Display a listing of the resource.
+//     *
+//     * @return \Illuminate\Http\Response
+//     */
 
     public function __construct()
     {
@@ -22,7 +25,22 @@ class OwnersController extends Controller
     public function index()
     {
         //
-        dd('オーナー一覧です');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+//        echo $date_now->year;
+//        echo $date_parse;
+
+        $e_all =Owner::all();
+        $q_get = DB::table('owners')->select('name','created_at')->get();
+//        $q_first = DB::table('owners')->select('name')->first();
+
+//        $c_test = collect([
+//            'name' => 'テスト'
+//        ]);
+//
+//        var_dump($q_first);
+//        dd($e_all,$q_get,$q_first,$c_test);
+        return view('admin.owners.index',compact('e_all','q_get'));
     }
 
     /**
