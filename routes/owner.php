99276953a9bec5b995ee7af65aa06927db502d86
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-10 16:13:11 +0900

    imageの初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
 //use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Owner\ImageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,6 +34,9 @@ middleware('auth:owners')->group(function () {
     Route::get('edit/{shop}',[ShopController::class, 'edit'])->name('shops.edit');
     Route::post('update/{shop}',[ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images',ImageController::class)
+    ->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
