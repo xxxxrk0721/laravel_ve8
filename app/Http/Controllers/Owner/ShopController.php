9716f7982929e08d053d540c8cd04454c5829598
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-10 14:47:20 +0900

    InterventionImage

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -34,6 +35,7 @@ class ShopController extends Controller
 
     public function index()
     {
+//        phpinfo();
 //        $ownerId = Auth::id();
         $shops = Shop::where('owner_id', Auth::id())->get();
         return view('owner.shops.index',
@@ -51,7 +53,13 @@ class ShopController extends Controller
     {
         $imageFile = $request->image; //一時保存
         if(!is_null($imageFile) && $imageFile->isValid() ){
-            Storage::putFile('public/shops', $imageFile);
+//            Storage::putFile('public/shops', $imageFile);
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName. '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+//            dd($imageFile,$resizedImage);
+            Storage::put('public/shops/' . $fileNameToStore,$resizedImage );
         }
 
         return redirect()->route('owner.shops.index');
