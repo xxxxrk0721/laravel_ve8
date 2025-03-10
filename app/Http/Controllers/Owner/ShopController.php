20a0f241b8c5309bd3d4ef16228dcdd5903fef70
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-10 16:01:53 +0900

    shop edit update

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Owner;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
@@ -53,11 +54,26 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
+        $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'information' => ['required', 'string', 'max:1000'],
+            'is_selling' => ['required'],
+        ]);
         $imageFile = $request->image; //一時保存
         if(!is_null($imageFile) && $imageFile->isValid() ){
             $fileNameToStore = ImageService::upload($imageFile,'shops');
 
-            
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+            $shop->filename = $fileNameToStore;
+        }
+
+        $shop->save();
+
 //            Storage::putFile('public/shops', $imageFile);
 //            $fileName = uniqid(rand().'_');
 //            $extension = $imageFile->extension();
@@ -67,6 +83,9 @@ class ShopController extends Controller
 //            Storage::put('public/shops/' . $fileNameToStore,$resizedImage );
         }
 
-        return redirect()->route('owner.shops.index');
+        return redirect()
+            ->route('owner.shops.index')
+            ->with(['message' => '店舗情報を更新しました。',
+            'status' => 'info' ]);
     }
 }
