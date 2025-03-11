889d129351a98b17bd97301b34ea2c2ea555fbf5
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-11 13:48:01 +0900

    micromodal

---
+++ resources/views/components/select-image.blade.php
@@ -0,0 +1,56 @@
+@php
+    if($name === 'image1'){ $modal = 'modal-1';}
+    if($name === 'image2'){ $modal = 'modal-2';}
+    if($name === 'image3'){ $modal = 'modal-3';}
+    if($name === 'image4'){ $modal = 'modal-4';}
+@endphp
+<div class="modal micromodal-slide" id="{{$modal}}" aria-hidden="true">
+    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+        <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{$modal}}-title">
+            <header class="modal__header">
+                <h2 class="modal__title" id="{{$modal}}-title">
+                    ファイルを選択してください
+                </h2>
+                <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+            </header>
+            <main class="modal__content" id="{{$modal}}-content">
+                <div class="flex flex-wrap">
+                    @foreach($images as $image)
+                        <div class="w-1/4 p-4">
+{{--                            <a href="{{ route('owner.images.edit', ['image' => $image->id]) }}">--}}
+                                <div class="border rounded-md p-4">
+                                    <img class=“image” data-id="{{ $name }}_{{ $image->id }}"
+                                         data-file="{{ $image->filename }}"
+                                         data-path="{{ asset('storage/products/') }}"
+                                         data-modal="{{ $modal }}"
+                                         src="{{ asset('storage/products/' . $image->filename)}}” >
+{{--                                    <x-thumbnail :filename="$image->filename" type="products" />--}}
+                                    <div class="text-gray-700">
+                                        {{ $image->title }}
+{{--                                    </div>--}}
+                                </div>
+{{--                            </a>--}}
+                        </div>
+                    @endforeach
+                </div>
+{{--                <p>--}}
+{{--                    Try hitting the <code>tab</code> key and notice how the focus stays within the modal itself. Also, <code>esc</code> to close modal.--}}
+{{--                </p>--}}
+            </main>
+            <footer class="modal__footer">
+{{--                <button type="button" class="modal__btn modal__btn-primary">Continue</button>--}}
+                <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
+            </footer>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="flex justify-around items-center mb-4">
+    <a data-micromodal-trigger="{{$modal}}" href='javascript:;'>ファイルを選択</a>
+    <div class="w-1/4">
+        <img id="{{ $name }}_thumbnail" src="">
+    </div>
+</div>
+<input id="{{ $name}}_hidden" type="hidden" name="{{ $name }}" value="">
---
+++ resources/views/owner/products/create.blade.php
@@ -0,0 +1,89 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    <form method="post" action="{{ route('owner.products.store') }}">
+                        @csrf
+
+                        <div class="-m-2">
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <select name="category">
+                                        @foreach($categories as $category)
+                                            <optgroup label="{{ $category->name }}">
+                                                @foreach($category->secondary as $secondary)
+                                                    <option value="{{ $secondary->id}}" >
+                                                        {{ $secondary->name }}
+                                                    </option>
+                                        @endforeach
+                                        @endforeach
+                                    </select>
+{{--                                    <label for="name" class="leading-7 text-sm text-gray-600">店名※必須</label>--}}
+{{--                                    <input type="text" id="name" name="name" value="{{ $shop->name }}" required class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">--}}
+                                </div>
+                            </div>
+                            <x-select-image :images="$images" name="image1" />
+                            <x-select-image :images="$images" name="image2" />
+                            <x-select-image :images="$images" name="image3" />
+                            <x-select-image :images="$images" name="image4" />
+{{--                            <div class="p-2 w-1/2 mx-auto">--}}
+{{--                                <div class="relative">--}}
+{{--                                    <label for="information" class="leading-7 text-sm text-gray-600">店舗情報※必須</label>--}}
+{{--                                    <textarea id="information" name="information" rows="10" required class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="p-2 w-1/2 mx-auto">--}}
+{{--                                <div class="relative">--}}
+{{--                                    <div class="w-32">--}}
+{{--                                        <x-thumbnail :filename="$shop->filename" type="shops" />--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="p-2 w-1/2 mx-auto">--}}
+{{--                                <div class="relative">--}}
+{{--                                    <label for="image" class="leading-7 text-sm text-gray-600">画像</label>--}}
+{{--                                    <input type="file" id="image" name="image" accept=“image/png,image/jpeg,image/jpg” class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="p-2 w-1/2 mx-auto">--}}
+{{--                                <div class="relative flex justify-around">--}}
+{{--                                    <div><input type="radio" name="is_selling" value=1 class="mr-2" @if($shop->is_selling === 1) checked  @endif>販売中</div>--}}
+{{--                                    <div><input type="radio" name="is_selling" value=0 class="mr-2" @if($shop->is_selling === 0) checked  @endif>停止中</div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+                            <div class="p-2 w-full flex justify-around mt-4">
+                                <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="flex mx-auto text-black bg-yellow-500 !important border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">戻る</button>
+                                <button type="submit" class="flex mx-auto text-black bg-indigo-500 !important border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">登録する</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+    'use strict'
+    const images = document.querySelectorAll('.image') //全てのimageタグを取得
+    images.forEach(image => { // 1つずつ繰り返す
+    image.addEventListener('click', function(e){  // クリックしたら
+    const imageName = e.target.dataset.id.substr(0, 6) //data-idの6文字
+    const imageId = e.target.dataset.id.replace(imageName + '_', '') // 6文字カット
+    const imageFile = e.target.dataset.file
+    const imagePath = e.target.dataset.path
+    const modal = e.target.dataset.modal
+    // サムネイルと input type=hiddenのvalueに設定
+    document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+    document.getElementById(imageName + '_hidden').value = imageId
+    MicroModal.close(modal); //モーダルを閉じる
+    })
+    })
+    </script>
+</x-app-layout>
