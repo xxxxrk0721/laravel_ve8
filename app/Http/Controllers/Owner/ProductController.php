371cd6a3a5d95e30a67a0ed86f39f32aed3b582d
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-11 11:48:08 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -3,18 +3,42 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('product');
+            if(!is_null($id)){ // null判定
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId;
+                if($productId !== Auth::id()){ // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+
+            return $next($request);
+        });
+
+    }
     public function index()
     {
-        //
+//        $products = Owner::findOrFail(Auth::id())->shop->product;
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+            ->where('id', Auth::id())->get();
+
+//        dd($ownerInfo);
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
