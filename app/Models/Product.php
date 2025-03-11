30e6a03c89a74d3deed08c906c3326865e7bcd53
xxxxrk0721 <r.kanno.sakurabo@gmail.com>
2025-03-11 11:59:45 +0900

    stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Image;
 use App\Models\SecondaryCategory;
 use App\Models\Shop;
+use App\Models\Stock;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -25,5 +26,10 @@ class Product extends Model
     public function imageFirst()
     {
         return $this->belongsTo(Image::class,'image1','id');
+    }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
     }
 }
